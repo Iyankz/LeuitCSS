90d4f731cd70b51926caaaeb04a3216b0badadbb
Yayang Ardiansyah <39462527+Iyankz@users.noreply.github.com>
2026-02-01 04:49:43 +0700

    Delete

--- app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LeuitCSS v1.0.0 - Routes Package
-"""
-
-from app.routes.auth import auth_bp
-from app.routes.main import main_bp
-from app.routes.backup import backup_bp
-
-__all__ = ['auth_bp', 'main_bp', 'backup_bp']
